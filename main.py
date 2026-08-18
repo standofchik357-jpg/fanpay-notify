a6758845f844a5aaea4c2e5c9202f50c9f9dcd06
standofchik357-jpg <standofchik357@gmail.com>
2026-08-18 22:37:35 +0300

    fix render port

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import asyncio
+import os
+import threading
+
+from aiohttp import web
 
 from app.bot import start_bot
 
 
+async def health(request):
+    return web.Response(text="Bot is running")
+
+
+def start_web():
+    app = web.Application()
+    app.router.add_get("/", health)
+
+    web.run_app(
+        app,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 10000))
+    )
+
+
 async def main():
+
+    threading.Thread(
+        target=start_web,
+        daemon=True
+    ).start()
 
     await start_bot()
 
